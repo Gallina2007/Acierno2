f582584ceb38c0d2f1e5faf11128e99655690738
Gallina2007 <gallina.riccardo@iisgalvanimi.edu.it>
2025-04-04 10:59:28 +0000

    non c'è il css

--- progettopt2/app.py
+++ progettopt2/app.py
@@ -1,12 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request,redirect,url_for
 app = Flask(__name__)
 lista=["ciao","ciao2","ciao3","ciao4","ciao5"]
 @app.route("/")
 def home():
     return render_template("index.html",lista=lista)
 
+@app.route('/aggiungi', methods=['POST'])
+def aggiungi():
+    elemento = request.form['elemento']
+    #aggiunge alla lista
+    if elemento:
+        lista.append(elemento)
+    return redirect(url_for('home'))
+
+@app.route('/rimuovi/<int:indice>', methods=['POST'])
+def rimuovi(indice):
+    if 0 <= indice < len(lista):
+        lista.pop(indice)
+    return redirect(url_for('home'))
 
 
+@app.route("/pulisci", methods=['POST'])
+def pulisci():
+    lista.clear()
+    return redirect(url_for('home'))
 
 
 
